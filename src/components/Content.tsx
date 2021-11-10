689f8175626b3420656ec4a64c2968e390b94ce6
Andre Solla <andrebmsolla@gmail.com>
2021-11-09 21:06:06 -0300

    Application refactored into SideBar and Content components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,17 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  movies: Array<any>
+}
+
+export function Content({ movies }: ContentProps) {
   // Complete aqui
+
+  return (
+    <div className="movies-list">
+      {movies.map(movie => (
+        <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+      ))}
+    </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,28 @@
-export function SideBar() {
+import { Button } from './Button';
+interface SideBarProps {
+  selectedGenreId: number;
+  genres: Array<any>;
+  handleGenreIdChange: (id: number) => void;
+}
+
+export function SideBar({selectedGenreId, handleGenreIdChange: handleClickButton, genres }: SideBarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
